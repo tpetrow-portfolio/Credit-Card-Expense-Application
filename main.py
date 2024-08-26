c8c3356b5e698b466441173afe745fcc884e03e1
Tyler Petrow <tpetrow@wgu.edu>
2024-08-26 15:52:50 -0400

    Reads in CSV files 'expenditures.csv' and 'trip_information.csv' on Working Branch

--- main.py
+++ main.py
@@ -8,3 +8,12 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+# read in CSV files "expenditures.csv" and "trip_information.csv"
+expendituresDF = pd.read_csv('expenditures.csv')
+tripDF = pd.read_csv('trip_information.csv')
+
+# test that each csv was read correctly
+# print(expendituresDF.head())
+# print()
+# print(tripDF.head())

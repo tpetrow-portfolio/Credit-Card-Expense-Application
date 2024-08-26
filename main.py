c0c82c634e49576e44ebbb0146554d0dbb48666b
Tyler Petrow <tpetrow@wgu.edu>
2024-08-26 15:09:51 -0400

    Creates main.py with header information and necessary libraries.

---
+++ main.py
@@ -0,0 +1,10 @@
+# Tyler Petrow
+# ID: 011118169
+# WGU C964 - Computer Science Capstone
+# SIM3 Task 2: Capstone Project Design and Development
+# Company Credit Card Expense Reporting and Forecasting Application
+
+# import necessary libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
